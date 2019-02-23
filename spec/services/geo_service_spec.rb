f851ae9084ef52044ccf8222fee53a7014318274
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-23 15:15:46 -0700

    continues with tests for geo service and dark sky service

--- spec/services/geo_service_spec.rb
+++ spec/services/geo_service_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe GeoService do
-  it 'exists' do
+  it 'exists', :vcr do
     gs = GeoService.new("Denver, Co")
 
     expect(gs).to be_a(GeoService)
   end
-  it 'returns lat and lng for location' do
+  it 'returns lat and lng for location', :vcr do
     location = "Denver, CO"
     gs = GeoService.new(location)
 
