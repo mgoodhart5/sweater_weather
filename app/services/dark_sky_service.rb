8105abac2ee844de25ba2c957b07159c074417d4
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-23 15:17:46 -0700

    implements code for dark sky, reconfigures request with figaro

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,8 +1,26 @@
 class DarkSkyService
   attr_reader :lat, :lon
-  
+
   def initialize(lat, lon)
     @lat = lat
     @lon = lon
+  end
+
+  def weather_info
+    key = "#{ENV['DARKSKY_API_KEY']}"
+    get_json("/forecast/#{key}/#{@lat},#{@lon}")
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+
+  def conn
+    Faraday.new(:url => "https://api.darksky.net") do |f|
+      f.params["exclude"] = "minutely"
+      f.adapter Faraday.default_adapter
+    end
   end
 end
