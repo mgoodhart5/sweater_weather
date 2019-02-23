0afeb031799a9dd7b40cf197c044b74d03aa34cf
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-23 09:17:44 -0700

    adds tests for geoservice

--- spec/services/geo_service_spec.rb
+++ spec/services/geo_service_spec.rb
@@ -6,4 +6,11 @@ describe GeoService do
 
     expect(gs).to be_a(GeoService)
   end
+  it 'returns lat and lng for location' do
+    location = "Denver, CO"
+    gs = GeoService.new(location)
+
+    expect(gs.lat).to eq(39.7392358)
+    expect(gs.lon).to eq(-104.990251)
+  end
 end
