546bb7d54fae0c88968fbb314d0c574c1320bde7
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-24 13:09:51 -0700

    refactors controller and renames dark sky to be the more aptly named weather

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location = "Denver, CO"
-    gs = GeoService.new(location)
-    dss = DarkSkyService.new(gs.lat, gs.lon)
-    response = dss.weather_info
-    obj = DarkSky.new(response, location)
-
-    render json: ForecastSerializer.new(obj)
+    weather = WeatherFacade.new(params["location"])
+    render json: ForecastSerializer.new(weather.forecast)
   end
 end
