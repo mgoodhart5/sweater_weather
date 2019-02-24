b16d3ee9ad6c99265c430b017d4b39f1661cb6d9
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-24 10:10:46 -0700

    moves forward with testing flickr api

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -6,4 +6,15 @@ describe FlickrService do
 
     expect(fs).to be_a(FlickrService)
   end
+  it 'returns a a hash of photo information', :vcr do
+    fs = FlickrService.new(39.7392358, -104.990251)
+
+    expect(fs.photo_info).to be_a(Array)
+    expect(fs.photo_info.count).to eq(25)
+    expect(fs.photo_info.first).to have_key(:farm)
+    expect(fs.photo_info.first).to have_key(:server)
+    expect(fs.photo_info.first).to have_key(:id)
+    expect(fs.photo_info.first).to have_key(:secret)
+    expect(fs.photo_info.first).to have_key(:title)
+  end
 end
