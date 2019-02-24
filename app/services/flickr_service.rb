7daf3f82a32a6d676278216473379a6f70a99bc5
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-24 11:25:24 -0700

    creates methods for pulling out photo information and mapping it to a source url for api/v1/backgrounds?location=#{location} endpoint

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -5,7 +5,7 @@ class FlickrService
   end
 
   def photo_info
-    get_json("/services/rest/?method=flickr.photos.search&tags=skyline%2Cscenic%2Cphotography%2Coutdoors&safe_search=1&lat=#{@lat}&lon=#{@lon}&per_page=25&format=json&nojsoncallback=1&sort=relevance")
+    get_json("/services/rest/?method=flickr.photos.search&tags=skyline%2Cscenic%2Cphotography&safe_search=1&lat=#{@lat}&lon=#{@lon}&per_page=5&sort=date-taken-desc&format=json&nojsoncallback=1")
   end
 
   def get_json(url)
@@ -18,5 +18,14 @@ class FlickrService
       f.params["api_key"] = ENV['FLICKR_API_KEY']
       f.adapter Faraday.default_adapter
     end
+  end
+
+  def photo_url
+    "https://farm#{pic_hash[0][:farm]}.staticflickr.com/#{pic_hash[0][:server]}/#{pic_hash[0][:id]}_#{pic_hash[0][:secret]}_b.jpg"
+  end
+
+  def pic_hash
+    #shuffle this so that it is a different picture
+    photo_info
   end
 end
