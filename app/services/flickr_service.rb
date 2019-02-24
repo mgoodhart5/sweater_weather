f99cb900fbdaa9a3c770c44b45b9e2610e7f9925
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-24 10:10:57 -0700

    implements code for flickr api call

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -3,4 +3,20 @@ class FlickrService
     @lat = lat
     @lon = lon
   end
+
+  def photo_info
+    get_json("/services/rest/?method=flickr.photos.search&tags=skyline%2Cscenic%2Cphotography%2Coutdoors&safe_search=1&lat=#{@lat}&lon=#{@lon}&per_page=25&format=json&nojsoncallback=1&sort=relevance")
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)[:photos][:photo]
+  end
+
+  def conn
+    Faraday.new(:url => "https://api.flickr.com") do |f|
+      f.params["api_key"] = ENV['FLICKR_API_KEY']
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
