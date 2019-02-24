b5b604c4ff2e56331ad1bdd2bd63bb8161752736
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-24 16:54:29 -0700

    refactors test for refactored flickr service

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -17,15 +17,10 @@ describe FlickrService do
     expect(fs.photo_info.first).to have_key(:secret)
     expect(fs.photo_info.first).to have_key(:title)
   end
-  it 'returns a single photo url', :vcr do
+  it 'returns a single photo hash, count of 5', :vcr do
     fs_1 = FlickrService.new(39.7392358, -104.990251)
 
-    expect(fs_1.photo_url.class).to eq(String)
-  end
-  it 'returns a different photo url', :vcr do
-    fs_1 = FlickrService.new(38.2527, -85.7585)
-    fs_2 = FlickrService.new(39.7392358, -104.990251)
-
-    expect(fs_1.photo_url).to_not eq(fs_2.photo_url)
+    expect(fs_1.pic_collection).to be_a(Array)
+    expect(fs_1.pic_collection.count).to eq(5)
   end
 end
