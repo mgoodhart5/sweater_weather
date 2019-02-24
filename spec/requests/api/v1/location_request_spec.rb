180c3f4f450a0af8d764c2b240fa0ed61e6ca66b
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-23 22:27:58 -0700

    writes test for location request spec

--- spec/requests/api/v1/location_request_spec.rb
+++ spec/requests/api/v1/location_request_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe "Location API" do
-  it "sends a location" do
-    location= create(:location)
+  it "sends a location", :vcr do
+    location = create(:location)
 
     get "/api/v1/forecast?location=#{location["city"]},#{location["state"]}"
 
     expect(response).to be_successful
+    answer = JSON.parse(response.body)["data"]["attributes"]
+
+    expect(answer).to be_a(Hash)
   end
 end
