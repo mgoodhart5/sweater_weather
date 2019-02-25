7bd51f93e630a3573f1f213907f34f3191f45137
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-25 12:03:50 -0700

    leaves comments in giphy facade. not enumerating properly, creating too many arrays with all objects having the same giphy for each array within the main array.

--- app/facades/giphy_facade.rb
+++ app/facades/giphy_facade.rb
@@ -17,11 +17,15 @@ class GiphyFacade
   end
 
   def all_giphys
-    giphy_call.giphy_info.map do |g_info|
+    giphy_call.giphy_info[0..7].map do |g_info|
       forecast_call.weather_info[:daily][:data][0..7].map do |weather_info|
         Giphy.new(g_info[:images][:fixed_width][:url], weather_info[:time], weather_info[:summary])
       end
     end.first
-    #this is gross and I am aware of how ridiculous it is, also it
+    #this is gross and I am aware of how ridiculous it is, also it doesn't actually work
+    #because i am being too wild with the enumerables
+    #it's mapping too many times, and incorrectly
+    #moving forward i would figure this out properly with a different and better way of doing it,
+    #i would pry in and see what and why it is doing what it is doing. my assumptions are incorrect currently
   end
 end
