104a6d30ae7fc9ff39dab28dea11b4c457c8d1dd
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-26 14:35:19 -0700

    favorite model and self method. all tests currently passing

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,12 @@
 class Favorite < ApplicationRecord
   belongs_to :user
+
+  def self.current_weather
+    Favorite.all.map do |fav|
+      gs = GeoService.new(fav.location)
+      fav.update(current_weather: DarkSkyService.new(gs.lat, gs.lon).weather_info[:currently])
+    end
+    Favorite.all
+  end
+
 end
