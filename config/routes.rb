aa8136971442bc81209e52f266f7e60f0dd1aee6
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-26 15:59:39 -0700

    adds route for delete favorites

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       post 'sessions', to: "sessions#create"
       post 'favorites', to: "favorites#create"
       get 'favorites', to: "favorites#index"
+      delete 'favorites', to: "favorites#destroy"
     end
   end
 end
