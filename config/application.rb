4e71536f937d96f14c2a14fa017218c3ac2f07cb
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-11 17:51:26 -0600

    gem and configuration of rack-cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,12 @@ module SweaterWeather
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
