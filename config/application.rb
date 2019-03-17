61357e5ff0bc96a8a2a3d8de3dd3e2cdb6767b17
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-17 12:42:39 -0600

    adds delete to cors config

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module SweaterWeather
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :options]
       end
     end
     # Don't generate system test files.
